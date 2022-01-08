e0a68a98332d3556a797498bafc694d157ba0641
Mohit Gupta <guptamm.22@gmail.com>
2022-01-08 15:11:22 +0530

    Navigation Menu

---
+++ app/src/main/java/com/custom/navigation/drawer/HomeActivity.java
@@ -0,0 +1,139 @@
+package com.custom.navigation.drawer;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.google.android.material.navigation.NavigationView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    NavigationView navigationView;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        navigationView = findViewById(R.id.navigation_menu);
+        navigationView.setItemIconTintList(null);
+        drawerLayout = findViewById(R.id.drawer);
+
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+
+                    case R.id.facebook:
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        startActivity(new Intent(Intent.ACTION_VIEW,
+                                Uri.parse("https://facebook.com")));
+                        break;
+
+                    case R.id.instagram:
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        startActivity(new Intent(Intent.ACTION_VIEW,
+                                Uri.parse("https://instagram.com")));
+                        break;
+
+                    case R.id.nav_rate:
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        try {
+                            startActivity(new Intent(Intent.ACTION_VIEW,
+                                    Uri.parse("market://details?id=" + getPackageName())));
+
+                        } catch (ActivityNotFoundException e) {
+                            startActivity(new Intent(Intent.ACTION_VIEW,
+                                    Uri.parse("http://play.google.com/store/apps/details?id=" + getPackageName())));
+                        }
+                        break;
+
+                    case R.id.nav_share:
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        Intent myIntent = new Intent(Intent.ACTION_SEND);
+                        myIntent.setType("text/plain");
+                        String shareBody = "https://play.google.com/store/apps/details?id=" + getPackageName();
+                        String shareSub = "Your Subject Here";
+                        myIntent.putExtra(Intent.EXTRA_SUBJECT, shareSub);
+                        myIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
+                        startActivity(Intent.createChooser(myIntent, "Share using"));
+                        break;
+
+                    case R.id.nav_privacy_policy:
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        startActivity(new Intent(Intent.ACTION_VIEW,
+                                Uri.parse("https://privacy.com")));
+                        break;
+
+                    case R.id.nav_terms_conditions:
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        startActivity(new Intent(Intent.ACTION_VIEW,
+                                Uri.parse("https://terms.com")));
+                        break;
+                }
+                return true;
+
+            }
+        });
+
+
+    }
+
+    // ACTION BAR MENU CODING WITHOUT ERROR
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.share_menu_home, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.share) {
+            Intent myIntent = new Intent(Intent.ACTION_SEND);
+            myIntent.setType("text/plain");
+            String shareBody = "https://play.google.com/store/apps/details?id=" + getPackageName();
+            String shareSub = "Your Subject Here";
+            myIntent.putExtra(Intent.EXTRA_SUBJECT, shareSub);
+            myIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
+            startActivity(Intent.createChooser(myIntent, "Share using"));
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("You really want to Exit?")
+                .setCancelable(true)
+                .setPositiveButton("Yes", (dialog, which) -> finish())
+                .setNegativeButton("No", (dialog, which) -> dialog.cancel())
+                .setNeutralButton("Our More Apps", (dialog, which) -> {
+                    Intent more_apps = new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/developer?id=Android"));
+                    startActivity(more_apps);
+                });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+}
